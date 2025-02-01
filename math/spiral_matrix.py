c1f9e3a1ebbed6555a16b40149b7cfa9041b22e3
Atanas <107621899+atanasivanovdev@users.noreply.github.com>
2025-02-01 23:11:11 +0100

    Implement spiral order traversal for a matrix

--- math/spiral_matrix.py
+++ math/spiral_matrix.py
@@ -14,8 +14,22 @@ from typing import List
 
 
 def spiralOrder(matrix: List[List[int]]) -> List[int]:
-    pass
-
+    result = []
+    while matrix:
+        result += matrix.pop(0)
+        
+        if matrix and matrix[0]:
+            for row in matrix:
+                result.append(row.pop())
+        
+        if matrix:
+            result += matrix.pop()[::-1]
+        
+        if matrix and matrix[0]:
+            for row in reversed(matrix):
+                result.append(row.pop(0))
+    
+    return result
 
 # Test cases
 # Example 1
