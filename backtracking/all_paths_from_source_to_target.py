3193074b696ef66ba3852b267dc559c42b4a250e
Atanas <107621899+atanasivanovdev@users.noreply.github.com>
2025-08-31 21:24:40 +0200

    Solve all paths from source to target problem

---
+++ backtracking/all_paths_from_source_to_target.py
@@ -0,0 +1,38 @@
+# Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
+
+# The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+
+"""
+Example 1:
+Input: graph = [[1,2],[3],[3],[]]
+Output: [[0,1,3],[0,2,3]]
+Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+
+Example 2:
+Input: graph = [[4,3,1],[3,2,4],[3],[4],[]]
+Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+"""
+
+def allPathsSourceTarget(graph):
+    """
+    :type graph: List[List[int]]
+    :rtype: List[List[int]]
+    """
+    def backtrack(node, path):
+
+        if node == len(graph) - 1:
+            res.append(path)
+            return
+        
+        for neighbor in graph[node]:
+            backtrack(neighbor, path + [neighbor])
+
+    res = []
+    backtrack(0, [0])
+    return res
+
+
+assert allPathsSourceTarget([[1,2],[3],[3],[]]) == [[0,1,3],[0,2,3]]
+assert allPathsSourceTarget([[4,3,1],[3,2,4],[3],[4],[]]) == [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+
+print("All test cases passed!")
