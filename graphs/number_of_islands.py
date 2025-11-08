7706942e8b68da29d0a5ad3dc6303fa16f5360a5
Atanas <107621899+atanasivanovdev@users.noreply.github.com>
2025-11-08 20:46:45 +0100

    Solve number of islands problem

---
+++ graphs/number_of_islands.py
@@ -0,0 +1,72 @@
+# Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+"""
+Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+"""
+
+def numIslands(grid):
+    """
+    :type grid: List[List[str]]
+    :rtype: int
+    """
+    if not grid:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    visited = set()
+
+    def dfs(r, c):
+        if (r < 0 or c < 0 or 
+            r >= rows or c >= cols or 
+            grid[r][c] == "0" or 
+            (r, c) in visited):
+            return
+
+        visited.add((r, c))
+        dfs(r + 1, c)
+        dfs(r - 1, c)
+        dfs(r, c + 1)
+        dfs(r, c - 1)
+
+    islands = 0
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == "1" and (r, c) not in visited:
+                dfs(r, c)
+                islands += 1
+
+    return islands
+
+assert numIslands([
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]) == 1
+
+assert numIslands([
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]) == 3
+
+print("All tests passed!")
